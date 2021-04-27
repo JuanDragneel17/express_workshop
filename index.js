421376a0233454b7b8badb167d28abd0e7496a31
Juan José González <juan_vegetto2013@hotmail.com>
2021-04-27 16:50:15 -0500

    JWT Implemented

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const pokemon  = require('./routes/pokemon');
 const user = require('./routes/user');
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound')
 
 /*
 Verbos HTTP
@@ -21,12 +23,11 @@ app.get("/", (req, res, next) => {
     return res.status(200).json({code: 1, message: "Bienvenidos a la Pokedex"});
 });
 
-app.use("/pokemon", pokemon);
 app.use("/user", user);
+app.use(auth);
+app.use("/pokemon", pokemon);
 
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"});
-});
+app.use(notFound);
 app.listen(process.env.PORT || 3000, () =>{
     console.log("Server is running...");
 });
