91991f0e754dfa982f0c3f833afef195489faaec
Juan José González <juan_vegetto2013@hotmail.com>
2021-04-29 19:56:32 -0500

    LOGIN

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//Dependencies
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+//Routers
 const pokemon  = require('./routes/pokemon');
 const user = require('./routes/user');
+//Middleware
 const auth = require('./middleware/auth');
-const notFound = require('./middleware/notFound')
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
 /*
 Verbos HTTP
@@ -15,14 +20,12 @@ PUT
 DELETE
 */
 
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res, next) => {
-    return res.status(200).json({code: 1, message: "Bienvenidos a la Pokedex"});
-});
-
+app.get("/", index);
 app.use("/user", user);
 app.use(auth);
 app.use("/pokemon", pokemon);
